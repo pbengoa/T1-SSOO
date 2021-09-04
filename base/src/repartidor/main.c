45ab8719784027ec003da99119249adb6c5ef676
pbengoa <pbengoa@uc.cl>
2021-09-03 20:50:57 -0400

    many changes

--- base/src/repartidor/main.c
+++ base/src/repartidor/main.c
@@ -3,7 +3,18 @@
 #include "repartidor.h"
 #include <stdlib.h>
 
+Repartidor* repartidor_init(int semaforo1, int semaforo2, int semaforo3, int bodega)
+{
+  Repartidor* repartidor = malloc(sizeof(Repartidor));
+  repartidor -> semaforo1 = semaforo1;
+  repartidor -> semaforo2 = semaforo2;
+  repartidor -> semaforo3 = semaforo3;
+  repartidor -> bodega = bodega;
+  repartidor -> position = 0;
+}
+
 int main(int argc, char const *argv[])
 {
   printf("I'm the REPARTIDOR process and my PID is: %i\n", getpid());
+  
 }
